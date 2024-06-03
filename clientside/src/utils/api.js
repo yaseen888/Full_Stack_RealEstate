cdb84ced0397f037d0271b074bbb813730a8e5d7
yaseen <yaseenfatima888@gmail.com>
2024-06-04 05:25:40 +0530

    new url changed

--- clientside/src/utils/api.js
+++ clientside/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import dayjs from "dayjs";
 import { toast } from "react-toastify";
 export const api = axios.create({
-  baseURL: "https://full-stack-real-estate-client-front-dxcjhyae7.vercel.app/", //specify the server here
+  baseURL: "https://full-stack-real-estate-client.vercel.app/", //specify the server here
 });
 //functions
 export const getAllProperties = async () => {
